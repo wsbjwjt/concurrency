a527a41ca2844892906cec808df2c7b2137b324a
wsbjwjt <wsbjwjt@gmail.com>
2024-12-08 09:09:21 +0800

    feature: support DashMap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -10,7 +10,7 @@ const M: usize = 4;
 fn main() -> Result<()> {
     let metrics = Metrics::new();
 
-    println!("{:?}", metrics.snapshot());
+    // println!("{:?}", metrics.snapshot());
 
     for idx in 0..N {
         task_worker(idx, metrics.clone())?;
